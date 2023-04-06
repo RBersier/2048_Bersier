1f613379767e86f95b150e12e94daf3a674da249
RBersier <127479397+RBersier@users.noreply.github.com>
2023-04-06 09:18:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 """
 Module : MA-20
 Titre : Jeu 2048
-Autheur : Ryan Bersier
-Date de dernière modification : 04.04.23
+Auteur : Ryan Bersier
+Date de dernière modification : 06.04.23
 Version : 1.0
 """
 
-# import des différentes bibliothèque
+# import des différentes librairies
 from tkinter import *
 from tkinter import messagebox
 import tkinter.font
 import random
 
-# definitiom des variable
+# definition des variables
 nb_move = 0
 temp_col = []
 nb_score = 0
@@ -41,7 +41,7 @@ table2 = [["", "", "", ""],
           ["", "", "", ""],
           ["", "", "", ""]]
 
-# définiton du tableau du 2048
+# definition du tableau du 2048
 labels = [[None, None, None, None],
           [None, None, None, None],
           [None, None, None, None],
@@ -69,7 +69,7 @@ def key_press(event):
         mix(True, 0)
 
 
-# check les mouvements disponible pour la grille
+# check les mouvements disponibles pour la grille
 def movement_checker():
     for row in range(len(table2)):
         for col in range(len(table2[row])):
@@ -153,7 +153,7 @@ def mix(rev, id):
 # fonction de calcul du highscore
 def highscore():
     global nb_highscore
-    # verifie si le score est supèrieur au highscore
+    # verifies si le score est supérieur au highscore
     if nb_score > nb_highscore:
         # ouvre le fichier data contenant le highscore et enregistre le nouveau score
         file = open("data.txt", "w")
@@ -166,7 +166,7 @@ def highscore():
     file.close()
 
 
-# actualise les diférente stat présente sur la partie graphique pendant la partie
+# actualise-les différentes stats présente sur la partie graphique pendant la partie
 def refresh_graphics():
     global score_label, highscore_label, movements_label, nb_score, nb_move, nb_highscore
 
@@ -175,15 +175,15 @@ def refresh_graphics():
     highscore_label.config(text=f"highscore : {nb_highscore}")
 
 
-# gère l'appariton de 2 et de 4
+# gère l'apparition de 2 et de 4
 def random_nb():
-    # definition d'une variable pour que si aucune case n'est disponible qu'il ne tente pas d'en regenerer des 2 ou 4
+    # definition d'une variable pour que si aucune case n'est disponible qu'il ne tente pas d'en régénérer des 2 ou des 4
     i = 0
     if movement_checker():
-        # definition d'une variable pour avoir 10% de chance qu'un 4 se gènère
+        # definition d'une variable pour avoir 10% de chance qu'un 4 se génère
         x = random.randint(0, 9)
         while True:
-            # choisie une case aléatoire parmis les 16
+            # choisie une case aléatoire parmis les 16 disponibles
             y = random.randint(0, 3)
             z = random.randint(0, 3)
             # verifie qu'il y est une case vide sur la grille
@@ -195,7 +195,7 @@ def random_nb():
             if i == 0:
                 break
             else:
-                # verifie que la case séléctionner est vide
+                # verifies que la case sélectionner est vide
                 if table2[y][z] == "":
                     # ajout de 4
                     if x == 0:
@@ -208,9 +208,9 @@ def random_nb():
     refresh()
 
 
-# pour les démmarage
+# pour les démarage
 def start_game():
-    # si la grille est vide génére 2 nombre sur la grille
+    # si la grille est vide généré 2 nombre sur la grille
     if table2 == [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]]:
         random_nb()
         random_nb()
@@ -219,18 +219,15 @@ def start_game():
 # gère le restart du jeu
 def reset_game(window=None):
     global nb_score, table2, nb_move, frame3, score_label, movements_label
-    # redéfinission des variable stat à 0 (sauf highscore)
+    # redéfinissions des variables stat à 0 (sauf highscore)
     nb_score = 0
     nb_move = 0
-    # vide la variable contenant la grille et la rédéfini avec rien dedans
+    # vide la variable contenant la grille et la redéfinie avec rien dedans
     table2.clear()
-    table2 = [["", "", "", ""],
-              ["", "", "", ""],
-              ["", "", "", ""],
-              ["", "", "", ""]]
+    table2 = [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]]
     refresh()
-    # pour la partie gagner detruit la fenêtre parallèle pour si le joueur choisie de recommencer une partie
-    if window != None:
+    # pour la partie gagner detruit la fenêtre parallèle pour si le joueur choisit de recommencer une partie
+    if window is not None:
         win_win_game.destroy()
     score_label.config(text=f"score : {nb_score}")
     movements_label.config(text=f"movements : {nb_move}")
@@ -243,53 +240,45 @@ def win_game():
     global w, win_win_game
     # definition de la variable gagné a faux tant que pas de 2048
     won = False
-    # verifie sur toutes les cases la présence d'une case 2048 si oui passe la variable gagné à vrai
+    # verifies sur toutes les cases la présence d'une case 2048 si oui passe la variable gagné à vrai
     for line in range(len(table2)):
         for col in range(len(table2[line])):
             if table2[col][line] == power2[10] and w == 0:
                 won = True
-    # si gagné créer une fenêtre au-dessus de la prmemière
+    # si gagné créer une fenêtre au-dessus de la première
     if won:
         win_win_game = Toplevel(win)
         win_win_game.geometry("800x150")
         win_win_game.title('gagner')
         win_win_game.configure(bg="grey")
-
         # frame du texte
         frame5 = Frame(win_win_game, bg="grey")
         frame5.pack()
         labels_win = Label(frame5, text="Congratulations you won!!!", font=("Helvetica", 24), bg="grey", fg="white")
         labels_win.pack()
-        labels_ask = Label(frame5,
-                           text="Now you have the choice to continue your game to go further than 2048, restart a game or leave the program",
-                           font=("Helvetica", 12), bg="grey", fg="white")
+        labels_ask = Label(frame5, text="Now you have the choice to continue your game to go further than 2048, restart a game or leave the program", font=("Helvetica", 12), bg="grey", fg="white")
         labels_ask.pack()
-
         # frame des 3 boutons
         frame6 = Frame(win_win_game, bg="grey")
         frame6.pack(pady=30)
         # bouton pour continuer la partie
-        continue_win = Button(frame6, text="Continue", font=("Helvetica", 12), bg="grey", fg="white",
-                              command=win_win_game.destroy)
+        continue_win = Button(frame6, text="Continue", font=("Helvetica", 12), bg="grey", fg="white", command=win_win_game.destroy)
         continue_win.pack(side=LEFT)
         # bouton pour recommencer une partie
-        reset_win = Button(frame6, text="Restart", font=("Helvetica", 12), bg="grey", fg="white",
-                           command=lambda: reset_game(win_win_game))
+        reset_win = Button(frame6, text="Restart", font=("Helvetica", 12), bg="grey", fg="white", command=lambda: reset_game(win_win_game))
         reset_win.pack(side=LEFT, padx=50)
         # bouton pour quitter le jeu
-        quit_win = Button(frame6, text="Leave", font=("Helvetica", 12), bg="grey", fg="white",
-                          command=win_win_game.destroy and win.destroy)
+        quit_win = Button(frame6, text="Leave", font=("Helvetica", 12), bg="grey", fg="white", command=win_win_game.destroy and win.destroy)
         quit_win.pack(side=LEFT)
-
         # win que 1 seule fois par partie
         w += 1
 
 
-# fonction pour la partie perdu
+# fonction pour la partie perdue
 def lose_game():
-    # racourci pour la fonction mouvement checker
+    # raccourci pour la fonction mouvement checker
     mc = movement_checker()
-    # verifie si plus aucun mouvement disponible
+    # verifies si plus aucun mouvement disponible
     if not mc:
         # pose la question au joueur s'il veut rejouer ou quitter le jeu
         answer = messagebox.askquestion("Lose", "You lose, you want to retry for reaching 2048 ?")
@@ -299,7 +288,7 @@ def lose_game():
             win.destroy()
 
 
-# définition de la fenêtre et ces paramètres
+# définition de la fenêtre et ces paramètres (le code ne peux pas fonctionner si cette partie se retrouve dans une fonction)
 if __name__ == '__main__':
     # window creation :
     win = Tk()
@@ -341,11 +330,9 @@ if __name__ == '__main__':
         # ajoute un label dans chaques case
         for line in range(len(table2)):
             for col in range(len(table2[line])):
-                labels[line][col] = tkinter.Label(frame1, text=table2[line][col], width=6, height=3, borderwidth=1,
-                                                  relief="solid", font=("Arial", 24), bg="lightgray")
+                labels[line][col] = tkinter.Label(frame1, text=table2[line][col], width=6, height=3, borderwidth=1, relief="solid", font=("Arial", 24), bg="lightgray")
                 # definition d'un décalage de la grille pour laisser un peu de place à la parti graphique
                 labels[line][col].grid(row=line + 5, column=col)
-
                 # défini les couleurs
                 try:
                     labels[line][col].config(bg=colors[table2[line][col]])
@@ -353,12 +340,10 @@ if __name__ == '__main__':
                         labels[line][col].config(fg="white")
                 except:
                     labels[line][col].config(bg="lightgray")
-
-        # gère la partie du highscore
-        highscore()
-
-        # gère la partie graphique du logiciel
-        refresh_graphics()
+            # gère la partie du highscore
+            highscore()
+            # gère la partie graphique du logiciel
+            refresh_graphics()
 
 
     # permet la détection d'appui de touche
